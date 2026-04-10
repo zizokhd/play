4f02402574e0117b1c16877c2cb0a297ac7d3910
zizokhd <149091615+zizokhd@users.noreply.github.com>
2026-04-10 18:50:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,17 +25,9 @@ forbidden_buttons = [
 # 📁 ملف البيانات
 data_FILE = "data.json"
 
-def load_data():
-    if not os.path.exists(DATA_FILE):
-        return {"users": {}, "orders": 0}
-    with open(DATA_FILE, "r") as f:
-        return json.load(f)
 
-def save_data(data):
-    with open(DATA_FILE, "w") as f:
-        json.dump(data, f)
 
-data = load_data()
+
 
 # 🧠 تخزين الطلبات المؤقت
 user_data = {}
